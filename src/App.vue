135069c83fb07d93ae845563c6e0e1f3e1a0ee44
nothosoo <hosoo.xoc@gmail.com>
2023-08-02 22:13:51 +0800

    feat: social link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import SocialLinks from '@/components/SocialLinks.vue'
 </script>
 
 <template>
   <RouterView />
+  <SocialLinks />
 </template>
